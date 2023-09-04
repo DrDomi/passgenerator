0dbe42aeb9a5382aa7156f9746d298173d8cf61f
domi <domiy888@gmail.com>
2023-09-04 15:31:27 +0300

    Minimal changes

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import random as ran
 
 def user_input():
-    lang_pwd = int(input("Введите желаемый язык пароля. Русский - 1, English - 2: ))
-    len_pwd = int(input("Введите желаемое количество слов в парольной фразе: ))
-    sep_pwd = input("Введите желаемый разделитель слов: )
+    lang_pwd = int(input("Введите желаемый язык пароля. Русский - 1, English - 2: "))
+    len_pwd = int(input("Введите желаемое количество слов в парольной фразе: "))
+    sep_pwd = input("Введите желаемый разделитель слов: ")
     return lang_pwd, len_pwd, sep_pwd
     
